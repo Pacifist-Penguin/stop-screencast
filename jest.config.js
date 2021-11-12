cc11056f0f93fa3c7d363b3cc2c6352a8f92ca21
NewPirateOfUASeas <75931217+NewPirateOfUASeas@users.noreply.github.com>
2021-11-12 14:00:17 +0300

    should no longer throw an error when importing a css file

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   transform: {
     '^.+\\.vue$': 'vue-jest'
+  },
+  moduleNameMapper: {
+    '^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub'
   }
 }
